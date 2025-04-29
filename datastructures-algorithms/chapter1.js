bcc8729e2f9769639dc1c852a68e6136f7ea9430
henrykimy1994 <henrykimy.1994@gmail.com>
2025-04-29 15:24:19 +0900

    chapter 1 content added

--- datastructures-algorithms/chapter1.js
+++ datastructures-algorithms/chapter1.js
@@ -112,3 +112,160 @@
         }
     }
 }
+{
+    // Classes
+    class Tree {
+        _children = [];
+
+        constructor(rootKey) {
+            this._key = rootKey;
+        }
+
+        isEmpty() {
+            return this._key === undefined;
+        }
+
+        get_Key() {
+            this._throwIfEmpty();
+            return this._key;
+        }
+
+        set_key(v) {
+            this._key = v;
+        }
+    }
+}
+{
+    // The spread operator
+    const myArray = [3, 3 ,4, 5, 5, 9, 2, 6];
+    const arrayMax = Math.max(...myArray);
+    const newArray = [...myArray] // new array
+
+    // build a copy of an object
+    const myObject = {first: 'Henry', year: 1894};
+    const newObject = {...myObject, last: 'Kim', year: 1994}
+
+    // undefined number of parameters
+    const myMax = (...nums) => {
+        let max = nums[0];
+        for (let i = i; i < nums.length; i++) {
+            if (max < nums[i]) max = nums[i];
+        }
+        return max;
+    };
+}
+{
+    // Destructuring statement
+    [first, second] = ['Henry', 'Kim'];
+
+    // Destructuring + spreading
+    [first, last, ...years] = ['Henry', 'Kim', 1994, 1894, 2001];
+
+    // default values
+    let [first, last, role = 'developer', experiences] = ['Henry', 'Kim']
+
+    // swap or rotating variables
+    [heap[p], heap[i]] = [heap[i], heap[p]];
+
+    // returning multiple values
+    const order2 = (a,b) => {
+        if (a<b) {
+            return [a, b];
+        } else {
+            return [b, a];
+        }
+    };
+    // array + destructuring more compact
+    let [smaller, bigger] = order2(20, 8);
+}
+{
+    // Modules
+    {
+        // CommonJS modules
+        const EOW = "m";
+        const newTree = () => null;
+
+        // black box software concept
+        // named export
+        module.exports = {
+            EOW, newTree
+        }
+        // const { EOW, newTree } = require("modulefile.js");
+        // OR
+        // const moduleObj = require("modulefile.js");
+        // moduleObj.EOW
+    }
+    {
+        // ECMAScript modules
+        // named export
+        const OMG = "omg";
+        export const addNew = 1 + 1;
+
+        export {
+            OMG,
+        };
+        //import { OMG, addNew } from 'modulefile.js';
+        // OR
+        // import * as moduleObj from 'modulefile.js';
+        // moduleObj.OMG
+    }
+}
+{
+    // Closures and immediately invoked function expressions
+    {
+        // equivalent of classes
+        function createPerson(firstName, lastName) {
+            let fName = firstName;
+            let lName = lastName;
+    
+            return {
+                getFirst: function () {
+                    return fName;
+                },
+                getLast: function () {
+                    return lName;
+                },
+                fullName: function () {
+                    return fName + " " + lName;
+                },
+                setName: function (firstName, lastName) {
+                    fName = firstName;
+                    lName = lastName;
+                }
+            };
+        }
+        const me = createPerson('Henry', 'Kim');
+        console.log(me.getFirst());
+        console.log(me.getLast());
+        console.log(me.fullName());
+    
+        me.setName("Chris", 'Kim');
+        console.log(me.fullName());    
+        // fName and lName are in the closure
+    }
+    {
+        // simulate modules using IIFE
+        const tax = (function (basicTax) { // nameless function
+            let vat = basicTax;
+
+            // ...other variables
+
+            return {
+                setVat: function (newVat) {
+                    vat = newVat;
+                },
+                getVat: function () {
+                    return vat;
+                },
+                addVat: function(value) {
+                    return value * (1 + vat / 100);
+                }
+                //other functions
+            }
+        })(6); // initial value passed as 6
+        // call it immediately - works like a module
+        console.log(tax.getVat());
+        tax.setVat(8);
+        console.log(tax.getVat());
+    }
+}
